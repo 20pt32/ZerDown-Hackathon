f82d2497ebd6c9a66f59e02e53ff12594e8bb4e4
20pt32 <76140010+20pt32@users.noreply.github.com>
2023-02-11 21:46:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,48 @@
 import tkinter as tk
-from tkinter import ttk
-import psycopg2
 
-conn = psycopg2.connect(database="postgres", user="postgres", password="sivapriya", host= "127.0.0.1" , port= "5432")
-cursor = conn.cursor()
+def get_top_agents_and_brokerages(market_id, n):
+    query = f"SELECT agent_info.id, agent_info.first_name, brokerage.name \
+             FROM agent_info \
+             LEFT JOIN brokerage ON agent_info.brokerage_id = brokerage.id \
+             WHERE agent_info.city = '{market_id}' \
+             ORDER BY agent_info.id \
+             LIMIT {n}"
+    # Connect to the database and execute the query
+    result = execute_query(query)
+    return result
 
+def display_result(result):
+    # Clear the previous result
+    result_text.delete('1.0', tk.END)
+    # Display the new result
+    for row in result:
+        result_text.insert(tk.END, f"Agent ID: {row[0]}, Agent Name: {row[1]}, Brokerage Name: {row[2]} \n")
 
-def get_top_agents(market_id, n):
-    cursor.execute("SELECT agent_id, name, sales FROM agent_info WHERE market_id = %s ORDER BY sales DESC LIMIT %s",
-                   (market_id, n))
-    return cursor.fetchall()
-
-
-def get_top_brokerages(market_id, n):
-    cursor.execute("SELECT brokerage_id, name, sales FROM brokerage WHERE market_id = %s ORDER BY sales DESC LIMIT %s",
-                   (market_id, n))
-    return cursor.fetchall()
-
-
+# Create the GUI window
 root = tk.Tk()
 root.title("Top Agents and Brokerages")
 
-market_id_label = ttk.Label(root, text="Market ID:")
-market_id_label.grid(row=0, column=0)
+# Create the input field for the market id
+market_id_label = tk.Label(root, text="Market ID:")
+market_id_entry = tk.Entry(root)
 
-market_id_entry = ttk.Entry(root)
-market_id_entry.grid(row=0, column=1)
+# Create the input field for the number of top agents and brokerages to display
+n_label = tk.Label(root, text="Number of Top Agents and Brokerages to Display:")
+n_entry = tk.Entry(root)
 
-n_label = ttk.Label(root, text="Number of agents/brokerages to display:")
-n_label.grid(row=1, column=0)
+# Create the submit button
+submit_button = tk.Button(root, text="Submit", command=lambda: display_result(get_top_agents_and_brokerages(market_id_entry.get(), int(n_entry.get()))))
 
-n_entry = ttk.Entry(root)
-n_entry.grid(row=1, column=1)
+# Create the result display field
+result_text = tk.Text(root, height=10, width=50)
 
-display_button = ttk.Button(root, text="Display", command=lambda: display_top(market_id_entry.get(), n_entry.get()))
-display_button.grid(row=2, column=0, columnspan=2, pady=10)
+# Place the widgets on the window
+market_id_label.grid(row=0, column=0, padx=10, pady=10)
+market_id_entry.grid(row=0, column=1, padx=10, pady=10)
+n_label.grid(row=1, column=0, padx=10, pady=10)
+n_entry.grid(row=1, column=1, padx=10, pady=10)
+submit_button.grid(row=2, column=0, padx=10, pady=10, columnspan=2)
+result_text.grid(row=3, column=0, padx=10, pady=10, columnspan=2)
 
-results_frame = ttk.LabelFrame(root, text="Results", height=200, width=300)
-results_frame.grid(row=3, column=0, columnspan=2)
-
-
-def display_top(market_id, n):
-    top_agents = get_top_agents(market_id, n)
-    top_brokerages = get_top_brokerages(market_id, n)
-
-    for i, agent in enumerate(top_agents):
-        agent_label = ttk.Label(results_frame, text="Agent {}: {} ({} sales)".format(i + 1, agent[1], agent[2]))
-        agent_label.grid(row=i, column=0)
-
-    for i, brokerage in enumerate(top_brokerages):
-        brokerage_label = ttk.Label(results_frame,
-                                    text="Brokerage {}: {} ({} sales)".format(i + 1, brokerage[1], brokerage[2]))
-        brokerage_label.grid(row=i, column=1)
-
-
+# Start the GUI event loop
 root.mainloop()
